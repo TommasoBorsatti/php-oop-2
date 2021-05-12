db7d8e318174782d0e947704f113a42c71a57e62
Tommaso Borsatti <tommasoborsatti89@gmail.com>
2021-05-12 16:19:04 +0200

    Aggiunta una seconda subclass, migliorata sintassi. Test su piu articoli

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <?php
 
 //classe madre
-class Products {
+class Product {
     //attributi
     public $name;
     public $price;
@@ -22,11 +22,20 @@ class Products {
         $this -> measures = $_measures;
     }
 
+    //metodi
+    public function isAvailable($item){
+        if ($item == true){
+            return "Disponibile in stock";
+        } else{
+            return "Al momento non disponibile";
+        }
+    }
+
    
 }
 
-//classe figlia
-class Clothes extends Products {
+//Subclasse
+class Clothe  extends Product {
 
     //attributi figli
     public $size;
@@ -49,29 +58,58 @@ class Clothes extends Products {
 
     }
 
-    //metodi
-    public function isAvailable($item){
-        if ($item == true){
-            return "Disponibile in stock";
-        } else{
-            return "Al momento non disponibile";
-        }
+    
+}
+
+//Subclasse
+class Sport extends Product {
+
+    //attributi figli
+    public $sport;
+    public $brand;
+    public $line;
+
+    //construct funcion
+    public function __construct(string $_name, float $_price, bool $_available, string $_productCode, float $_weight, string $_measures, string $_sport, string $_brand, string $_line)
+    {
+        parent::__construct($_name, $_price, $_available, $_productCode, $_weight, $_measures);
+        $this -> sport = $_sport;
+        $this -> brand = $_brand;
+        $this -> line = $_line;
+
     }
+
+    
 }
 
 
 //ISTANZE
+$miscellanous_1= new Product ("Sedia Kinski", 34, true, "ASAD4589", 4.2, "40x80x56");
+$miscellanous_2= new Product ("Vaso per piante Stuttgart", 8, true, "FH4658FJ", 0.1, "12x12x8");
 
-$clothe_1 = new Clothes("Fuss Beanie Hat", 22, true, "X145FFBB", 0.1, "22x10x15", "L", "Cappello a cuffia", "Grigio", "Unisex", "Casual", "Cotone 100%");
-$clothe_2 = new Clothes("Madrigals' Sweater Shirt", 66, false, "38FNNWF34", 0.8, "80x170x90", "M", "Felpa sportiva", "Blu Petrolio", "Uomo", "Sportswear", "Cotone 40% / Poliestere 60%");
+$clothe_1 = new Clothe ("Fuss Beanie Hat", 22, true, "X145FFBB", 0.1, "22x10x15", "L", "Cappello a cuffia", "Grigio", "Unisex", "Casual", "Cotone 100%");
+$clothe_2 = new Clothe ("Madrigals' Sweater Shirt", 66, false, "38FNNWF34", 0.8, "80x170x90", "M", "Felpa sportiva", "Blu Petrolio", "Uomo", "Sportswear", "Cotone 40% / Poliestere 60%");
+
+$sport_1 = new Sport ("Roteiro PRO", 60, false, "FHE74858", 0.2, "25x25x25", "Pallone da calcio", "Nike", "Nike Pro Futebol");
+$sport_2 = new Sport ("Spalding XS23", 58, true, "HR748FWI", 0.3, "29x29x29", "Pallone da basket", "Spalding", "NBA 4real");
 
 
 
 // ARRAY PER FOREACH PROVA+
 
+$miscellanous = [
+    $miscellanous_1,
+    $miscellanous_2,
+];
+
 $clothes = [
     $clothe_1,
     $clothe_2
+];
+
+$sports = [
+    $sport_1,
+    $sport_2
 ];
 
 ?>
@@ -85,10 +123,29 @@ $clothes = [
     <title>Store</title>
 </head>
 <body>
+
+    <h1>IL NOSTRO STORE</h1>
+
+    <h2>-GENERALI-</h2>
+
+    <?php foreach ($miscellanous as $item){ ?>
+    
+    <h3><?php echo $item -> name ; ?></h3>
+    <ul>
+        <li>Prezzo: <?php echo $item -> price ; ?> €</li>
+        <li>Disponibilità: <?php echo $item-> isAvailable($item->available) ; ?></li>
+        <li>Codice prodotto: <?php echo $item -> productCode ; ?></li>
+        <li>Peso in kg: <?php echo $item -> weight ; ?></li>
+        <li>Misure in cm: <?php echo $item -> measures ; ?></li>
+    </ul>
+
+    <?php } ?>
+
+    <h2>-ABBIGLIAMENTO-</h2>
     
     <?php foreach ($clothes as $clothe){ ?>
-
-    <h2><?php echo $clothe -> name ; ?></h2>
+    
+    <h3><?php echo $clothe -> name ; ?></h3>
     <ul>
         <li>Prezzo: <?php echo $clothe -> price ; ?> €</li>
         <li>Disponibilità: <?php echo $clothe-> isAvailable($clothe->available) ; ?></li>
@@ -102,7 +159,25 @@ $clothes = [
         <li>Trend: <?php echo $clothe -> style ; ?></li>    
         <li>Tessuto: <?php echo $clothe -> fabric ; ?></li>         
     </ul>
+
+    <?php } ?>
+
+    <h2>-ARTICOLI SPORTIVI-</h2>
     
+    <?php foreach ($sports as $sport){ ?>
+    
+    <h3><?php echo $sport -> name ; ?></h3>
+    <ul>
+        <li>Prezzo: <?php echo $sport -> price ; ?> €</li>
+        <li>Disponibilità: <?php echo $sport-> isAvailable($sport->available) ; ?></li>
+        <li>Codice prodotto: <?php echo $sport -> productCode ; ?></li>
+        <li>Peso in kg: <?php echo $sport -> weight ; ?></li>
+        <li>Misure in cm: <?php echo $sport -> measures ; ?></li>
+        <li>Tipo di attrezzatura: <?php echo $sport -> sport ; ?></li>
+        <li>Marca: <?php echo $sport -> brand ; ?></li>
+        <li>Linea: <?php echo $sport -> line ; ?></li>
+    </ul>
+
     <?php } ?>
     
 
